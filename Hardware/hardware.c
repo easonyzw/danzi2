033c3c437d6ae348711effbeb97ee66724aadcae
easonyzw <101691537+easonyzw@users.noreply.github.com>
2026-05-06 21:36:17 +0800

    Initialize buzzer hardware

--- Hardware/hardware.c
+++ Hardware/hardware.c
@@ -2,10 +2,12 @@
 #include "Timer.h"
 #include "ESP01S.h"
 #include "dht11.h"
+#include "Buzzer.h"
 
 void Hardware_Init(void)
 {
     Timer_Init();
     ESP01S_Init();
     DHT11_Init();
+    Buzzer_Init();
 }
