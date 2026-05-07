e636e7b743fa814f4c2c55214a77e7b2a27bf5aa
easonyzw <101691537+easonyzw@users.noreply.github.com>
2026-05-07 19:38:51 +0800

    Update esp01s.h

--- Hardware/esp01s.h
+++ Hardware/esp01s.h
@@ -7,6 +7,27 @@ void ESP01S_Init(void);
 void ESP01S_SendByte(uint8_t byte);
 void ESP01S_SendString(char *str);
 
-void ESP01S_SendData(uint8_t temperature, uint8_t humidity);
+/*
+ * 发送给 ESP01S 的数据格式：
+ *
+ * {
+ *   "EnvironmentTemperature":25.0,
+ *   "EnvironmentHumidity":60.0,
+ *   "Latitude":40.87,
+ *   "Longitude":111.68,
+ *   "Altitude":0.00
+ * }
+ *
+ * 注意：
+ * latitude_x1000000  = 纬度 * 1000000
+ * longitude_x1000000 = 经度 * 1000000
+ * altitude_x100      = 海拔 * 100
+ */
+void ESP01S_SendData(uint8_t temperature,
+                     uint8_t humidity,
+                     int32_t latitude_x1000000,
+                     int32_t longitude_x1000000,
+                     int32_t altitude_x100,
+                     uint8_t gps_valid);
 
 #endif
