0936b8542b9cfb37f872e2be5e6fe363b4d75a7e
easonyzw <101691537+easonyzw@users.noreply.github.com>
2026-05-09 17:51:28 +0800

    Update data.c

--- APP/data.c
+++ APP/data.c
@@ -1,6 +1,22 @@
 #include "Data.h"
 
-SensorData_t g_sensor = {
-    .temperature = 25,
-    .humidity = 60
+/*
+ * 当前传感器数据
+ * 初始值给 0
+ */
+SensorData_t g_sensor =
+{
+    0,      // temperature
+    0       // humidity
+};
+
+/*
+ * 系统设置参数
+ * 默认温度阈值：30℃
+ * 默认湿度阈值：60%
+ */
+SystemSetting_t g_setting =
+{
+    30,     // temp_threshold
+    60      // humi_threshold
 };
